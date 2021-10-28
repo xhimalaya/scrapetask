131d1a10ec144938c514165efa66bd3fcd6fcd10
Himalaya <himalaya.mandal@acronotics.com>
2021-10-28 08:05:52 +0530

    update of .....

--- main.py
+++ main.py
@@ -22,21 +22,25 @@ try:
     driver.get("https://food.grab.com/ph/en/")
     time.sleep(5)
     insert_address = driver.find_element_by_xpath('//*[@id="location-input"]')
-    print("<><><>>>>>>  ", insert_address)
-    insert_address.send_keys("Philippines")
+    insert_address.send_keys("Thailand")
     time.sleep(2)
     insert_address.send_keys(Keys.ENTER)
     time.sleep(2)
     driver.find_element_by_xpath('//button[@class="ant-btn submitBtn___2roqB ant-btn-primary"]').click()
     time.sleep(2)
+    raw_page_source= ""
     while True:
         try:
             driver.find_element_by_xpath('//button[@class="ant-btn ant-btn-block"]').click()
-            scrape.scrapelocation(driver.page_source)
+            print(">>>>>>>>>>>>>  ", len(driver.page_source))
+            raw_page_source = driver.page_source
         except Exception as e:
             print("I think all page load is completed", e)
             break
-        time.sleep(1)
+        finally:
+            raw_source = driver.page_source
+        time.sleep(2)
+    driver.quit()
+    # scrape.scrapelocation(raw_source)
 except Exception:
     pass
-driver.quit()
--- scrape.py
+++ scrape.py
@@ -26,10 +26,18 @@ def ProxyDns():
         return secure_random.choice(proxies)
 
 def scrapelocation(data):
-    soup = BeautifulSoup(data)
-    raw_json_data=json.loads(str(soup.find("script", {"id":"__NEXT_DATA__"})).replace('<script id="__NEXT_DATA__" type="application/json">','').replace("</script>",'').replace('\\u0026',','))
-    for resturant_id, details in raw_json_data['props']["initialReduxState"]["pageRestaurantsV2"]["entities"]["restaurantList"].items():
-        print("name >>>  ", details["name"])
-        print("latitude >>>  ", details["latitude"])
-        print("longitude >>>  ", details["longitude"])
-        print("*"*60)
+    soup = BeautifulSoup(data, features="html.parser")
+    all_div_tab = soup.find_all("div", {"class": "ant-col-24 RestaurantListCol___1FZ8V ant-col-md-12 ant-col-lg-6"})
+    print(">>>>>>  <<<<<<< ", len(all_div_tab))
+    for url_soup in all_div_tab:
+        resturent_url="https://food.grab.com/"+(url_soup.find("a")["href"])
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+
+        resturant_source = requests.get(resturent_url, headers=headers)
+        sub_soup = BeautifulSoup(resturant_source.text, features="html.parser")
+        raw_json_data=json.loads(str(sub_soup.find("script", {"id":"__NEXT_DATA__"})).replace('<script id="__NEXT_DATA__" type="application/json">','').replace("</script>",'').replace('\\u0026',','))
+        geo_data = raw_json_data['props']["initialReduxState"]["geolocation"]["currentLocation"]
+        print("latitude >> ", geo_data["latitude"])
+        print("longitude >> ", geo_data["longitude"])
+        print("name >>>>  ", sub_soup.find("h1",{"class":"name___1Ls94"}).text)
+        print("_"*60)
